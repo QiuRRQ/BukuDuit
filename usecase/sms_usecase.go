85d72484d12562238bb6ace3af37b0ce1b5fa12a
syaikhul hadi <my.ant2008@gmail.com>
2020-06-17 02:18:35 +0700

    amqp listener otp

---
+++ usecase/sms_usecase.go
@@ -0,0 +1,19 @@
+package usecase
+
+import (
+	"os"
+)
+
+type SmsUseCase struct {
+	*UcContract
+}
+
+func (uc SmsUseCase) SendSms(message,receiver string) (err error) {
+	sender := os.Getenv("TWILLIO_TESTING_PHONE")
+	_,_,err = uc.Twilio.SendSMS(sender,receiver,message,"","")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
